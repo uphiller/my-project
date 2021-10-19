ec4e569f227ff2c6afd61a4c922654a6a60b5c55
ho <jason.uphiller@gmail.com>
2021-10-19 17:08:36 +0900

    eb 배포 위한 소스 변경

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello World!111111222'
+    return 'Hello World!111111222555'
 
 
 if __name__ == '__main__':
